61ff84d85abb52656ad56fc65529e681fc1be83e
itamarc101 <45041126+itamarc101@users.noreply.github.com>
2024-04-13 11:33:08 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
-from tkinter import Tk, Label, Entry, Button, messagebox
+from tkinter import Tk, Entry, Button, messagebox, Label, Canvas
+from PIL import Image, ImageTk
 import requests
+import os
 
 def classify_url():
     url = url_entry.get()
@@ -17,16 +19,51 @@ def classify_url():
     except requests.exceptions.RequestException as e:
         messagebox.showerror("Error", f"Error: {str(e)}")
 
+
+def resize_bg_image(event):
+    """
+    Resize the background image to match the window size.
+    
+    Parameters:
+        event (Event): The resizing event.
+    """
+    global bg_image_resized
+    global bg_image_tk  # Declare global variable for bg_image_tk
+    global canvas  # Declare global variable for canvas
+    # Resize the background image
+    bg_image_resized = bg_image.resize((event.width, event.height), Image.LANCZOS)
+    bg_image_tk = ImageTk.PhotoImage(bg_image_resized)
+    canvas.config(width=event.width, height=event.height)  # Update canvas size
+    canvas.create_image(0, 0, anchor="nw", image=bg_image_tk)
+
+
 root = Tk()
 root.title("URL Classifier")
 
+# Set initial size of the window
+root.geometry("512x256")
+
+# Load the background image
+script_dir = os.path.dirname(os.path.abspath(__file__))
+bg_image = Image.open(os.path.join(script_dir, 'img.png'))
+bg_image_resized = bg_image.resize((root.winfo_screenwidth(), root.winfo_screenheight()), Image.LANCZOS)
+bg_image_tk = ImageTk.PhotoImage(bg_image_resized)
+
+# Create a Canvas widget to display the background image
+canvas = Canvas(root, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
+canvas.pack(fill="both", expand=True)
+canvas.create_image(0, 0, anchor="nw", image=bg_image_tk)
+
+# Bind the resize function to the window resizing event
+root.bind('<Configure>', resize_bg_image)
+
 # Create GUI components (Label, Entry, Button)
-Label(root, text="Enter URL:").grid(row=0, column=0)
+Label(root, text="Enter URL:", bg='white').place(x=50, y=50)
 url_entry = Entry(root, width=50)
-url_entry.grid(row=0, column=1)
+url_entry.place(x=150, y=50)
 classify_button = Button(root, text="Classify", command=classify_url)
-classify_button.grid(row=1, column=0, columnspan=2)
+classify_button.place(x=230, y=100)
 result_label = Label(root, text="")
-result_label.grid(row=2, column=0, columnspan=2)
+result_label.place(x=50, y=100)
 
 root.mainloop()
